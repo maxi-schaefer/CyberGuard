20ee907514525fe0e595d1d87a4f3c71d8e8cd21
Maxi Schäfer <schaefer.maxi0910@icloud.com>
2026-02-05 22:54:04 +0100

    Started with frontend

---
+++ components/dashboard/dashboard.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+import { Skeleton } from "../ui/skeleton";
+
+function DashboardSkeleton() {
+    return (
+        <div className="flex flex-col gap-6 p-6">
+            <div className="grid grid-cols-2 gap-4 lg:grid-cols-4">
+                {Array.from({ length: 4 }).map((_, i) => (
+                    <Skeleton key={i} className="h-24 rounded-lg bg-secondary" />
+                ))}
+            </div>
+
+            <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+                <Skeleton className="h-75 rounded-lg bg-secondary lg:col-span-2" />
+                <Skeleton className="h-75 rounded-lg bg-secondary" />
+            </div>
+
+            <Skeleton className="h-100 rounded-lg bg-secondary" />
+        </div>
+    )
+}
+
+export function Dashboard() {
+    const [isLoading, setIsLoading] = useState(true) // For now, later use useSWR and call api
+    
+    return (
+        <div className="flex min-h-screen flex-col bg-background">
+            { isLoading ? (<DashboardSkeleton />) : (<>Hello World</>)}
+        </div>
+    )
+}
